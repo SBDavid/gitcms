c4a670ef5c47911dd9428c7b0ce4f7feb6437be5
davidtangjw <davidtangjw@pptv.com>
2017-09-12 10:27:48 +0800

    update

--- src/persistence/api/page.js
+++ src/persistence/api/page.js
@@ -1,12 +1,11 @@
 var pageModel = require('../schema/page');
 var sha1 = require('node-sha1');
 
-
 module.exports = {
     insert: function(page, callback) {
         var newPageModel = new pageModel(page);
 
-        newPageModel.id = sha1(JSON.stringify(page));
+        newPageModel.id = sha1(JSON.stringify(newPageModel));
 
         newPageModel.save(callback);
     }
